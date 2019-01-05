dfc4f68d13c9173e8a3977ca6ab6bb3e110c51ba
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2019-01-05 21:22:15 +0100

    $@

--- js/main.js
+++ js/main.js
@@ -10,13 +10,15 @@ let home_page = document.getElementById('home-page');
 let debt_page = document.getElementById('debt-page');
 let history_page = document.getElementById('history-page');
 let footer = document.getElementById('footer');
-let stayLogIn = sessionStorage.getItem('key');
+let getKey = sessionStorage.getItem('key');
 let logged_user = document.getElementById('logged-user');
 let user = sessionStorage.getItem('user');
 let history_container = document.getElementById('history-container');
+let balance = document.getElementById('balance');
+let message = document.getElementById('message');
 logged_user.innerHTML = user;
 
-if (stayLogIn == '123') {
+if (getKey == '123') {
     login_page.classList.add("is-close");
     header.classList.remove("is-close");
     footer.classList.remove('is-close');
@@ -51,12 +53,11 @@ submit_debt.addEventListener('click', (e) => {
     let debtor = document.getElementById('debtor').value;
     let lendor = document.getElementById('lendor').value;
     let amount = document.getElementById('amount').value;
-    let desc = document.getElementById('description').value;
-    var validation = sessionStorage.getItem('key');
-    if (validation == '123') {
+    let desc = document.getElementById('description').value;   
+    if (getKey == '123') {
         addSomeNewData(debtor, lendor, amount, desc);
         add_debt_page.classList.add('is-close');        
-
+        ShowTheDebts();
 
     }
 })
@@ -65,6 +66,7 @@ submit_box.addEventListener('click', (e) => {
     let login = document.getElementById('login_box').value.toLowerCase();
     let password = document.getElementById('password_box').value;
     CheckTheData(login, password);
+    ShowTheDebts();
 })
 
 function CheckTheData(l, p) {
@@ -83,20 +85,23 @@ function sendData(data, l, p) {
             console.log(element);
             valid = true;
             key_value = element.key;            
-            user_value = element.Login.toUpperCase();
+            user_value = element.Login.toUpperCase();  
+            
         }
     })
     if (valid) {
         alert('Hello ' + l.toUpperCase());
+        sessionStorage.setItem('key', key_value);
+        sessionStorage.setItem('user',user_value);
+        location.reload();
         login_page.classList.add("is-close");
         header.classList.remove("is-close");
         footer.classList.remove('is-close');
         home_page.classList.remove("is-close");
         debt_page.classList.remove('is-close');
-        history_page.classList.remove('is-close');
-        sessionStorage.setItem('key', key_value);
-        sessionStorage.setItem('user',user_value);        
+        history_page.classList.remove('is-close');                
         logged_user.innerHTML = user_value;
+        
         
         
         
@@ -136,15 +141,19 @@ function addSomeNewData(debtor_val, lender_val, amount_val, desc_val) {
 
 
 /////////////////////////////////////////////////////////////////////////////////////////
+//<------HISTORY PAGE RENDERING------>
 function ShowTheDebts() {
-
+    if(getKey == '123'){
         const url = "https://7kkvlvmf39.execute-api.eu-central-1.amazonaws.com/development/myHistoryLambda";
         fetch(url)
             .then(response => response.json())
             .then(data => showData(JSON.parse(data.query)));
-            
+    }
+    else{
+        history_container.innerHTML = "YOU ARE NOT ALLOWED TO SEE THIS CONTENT";
+    }
     };
-//<------HISTORY PAGE RENDERING------>
+
 let containerMarkup = '';
 let historyMap = [];
 function showData(data){
@@ -156,20 +165,17 @@ data.forEach((e) => {
     temp.amount = e.amount;
     temp.desc = e.description;
     temp.date = e.CreationDate;
-    historyMap.push(temp);
-    console.log(temp);
+    historyMap.push(temp);   
 });
 containerMarkup = `<ul class='history-class'>`;
  
     
 historyMap.forEach((u) => {
-  containerMarkup += `<li class='history-li-class'> <p>ID:</p> ${u.id}  <p>Debtor:</p> ${u.debtor}  <p>Lender:</p> ${u.lender}  <p>Amount:</p> ${u.amount}<br><p>Desc:</p> ${u.desc}  <p>Date:</p> ${u.date}</li>` ;
-     console.log(u);
+  containerMarkup += `<li class='history-li-class'> <p>ID:</p> ${u.id}  <p>Debtor:</p> ${u.debtor}  <p>Lender:</p> ${u.lender}  <p>Amount:</p> ${u.amount}<p>Desc:</p> ${u.desc}  <p>Date:</p> ${u.date}</li><br>` ;     
 });
 
 containerMarkup += '</ul>';
 history_container.innerHTML = containerMarkup;
-console.log(containerMarkup);
 }
 ShowTheDebts();
 
@@ -216,8 +222,7 @@ users.forEach((user, index) => {
 });
 
 
-userMap.forEach((u) => {
-    console.log(u);
+userMap.forEach((u) => {    
     listMarkup += `<li>
                     <div class='accordion-header'>
                         <p>${u.user}<span class="balance positive"> 0.00$</span></p>
@@ -240,3 +245,56 @@ userMap.forEach((u) => {
 });
 
 debtList.innerHTML = listMarkup;
+//<------------HOME PAGE RENDERING----------->
+function ShowTheHomePageDebt() {
+    if(getKey == '123'){
+        const url = " https://7kkvlvmf39.execute-api.eu-central-1.amazonaws.com/development/transactionHistory";
+        fetch(url)
+            .then(response => response.json())
+            .then(data => showHomeData(JSON.parse(data.query)));
+    }
+    else{
+        home_page.innerHTML = "YOU ARE NOT ALLOWED TO SEE THIS CONTENT";
+    }
+    };
+
+
+
+let containerHomeMarkup = '';
+let homeMap = [];
+let balance_amount = 0;
+function showHomeData(data){
+data.forEach((e) => {
+    if(e.debtor == user.toLowerCase()){        
+        balance_amount -= e.amount;             
+    }
+    else if(e.lender == user.toLowerCase()){
+        balance_amount += e.amount;             
+    }      
+    let temp = []; 
+    temp.balance_amount = balance_amount;      
+    homeMap.push(temp);   
+});
+var lastRowOfHomeMap = homeMap[homeMap.length - 1];
+balance.innerHTML = lastRowOfHomeMap.balance_amount;
+if(lastRowOfHomeMap.balance_amount > 0){
+    message.innerHTML = "It`s look like you lend someone yours money.."
+}
+if((lastRowOfHomeMap.balance_amount < 0)){
+    message.innerHTML = "It`s look like you own some money..."
+}
+if(lastRowOfHomeMap.balance_amount == 0){
+    message.innerHTML = "It`s look like you gucci with money..."
+}
+containerHomeMarkup = `<ul class='history-class'>`;
+ 
+    
+homeMap.forEach((u) => {
+  containerHomeMarkup += `<li class='history-li-class'> <p>ID:</p> ${u.id}  <p>Debtor:</p> ${u.debtor}  <p>Lender:</p> ${u.lender}  <p>Amount:</p> ${u.amount}<p>Desc:</p> ${u.desc}  <p>Date:</p> ${u.date}</li><br>` ;     
+});
+
+containerHomeMarkup += '</ul>';
+//home_container.innerHTML = containerMarkup;
+}
+ShowTheHomePageDebt();
+
